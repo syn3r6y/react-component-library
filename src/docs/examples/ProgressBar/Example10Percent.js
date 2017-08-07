ec75d301bc18f5963570817da6d98e078b4c420b
syn3r6y <syn3rgy@live.ca>
2017-08-06 20:31:20 -0600

    adds progress bar component

---
+++ src/docs/examples/ProgressBar/Example10Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'base-react/ProgressBar';
+
+/** 10% progress */
+const Example10Percent = () => <ProgressBar percent={10} width={150} />;
+
+export default Example10Percent;
---
+++ src/docs/examples/ProgressBar/Example70Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'base-react/ProgressBar';
+
+/** 70% progress */
+const Example70Percent = () => <ProgressBar percent={70} width={150} />;
+
+export default Example70Percent;
